7e59207734ec7e8c5b3193b7b9972367b625da82
Chanda Shrestha <chandashrestha89@gmail.com>
2018-10-22 19:56:28 -0500

    setup

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.c2b00400d033471fa83e7c48889d19d1,
+  secret: process.env.e4de6de01b88427ba0f0c7485a2978c7
 };
