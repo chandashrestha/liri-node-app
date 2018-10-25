302d66e718beb21aa916b7eee911ce64729da3a9
Chanda Shrestha <chandashrestha89@gmail.com>
2018-10-25 14:15:25 -0500

    random.txt

--- liri.js
+++ liri.js
@@ -20,75 +20,89 @@ var spotify = new SpotifyAPI(keys.spotify);
 var command = process.argv[2];
 var input = process.argv[3];
 
-if (process.argv[2] === "spotify-this-song") {
-  //spotify search requires
-  var query = {
-    type: "track",
-    query: input
-    // limit: 1
-  };
+doSomething(command, input);
 
-  spotify.search(query, function(err, data) {
-    //   console.log(data.tracks.items);
-    console.log(
-      "Artist(s): " +
-        data.tracks.items[0].artists[0].name +
-        "\nSong's Name: " +
-        data.tracks.items[0].name +
-        "\nPreviewUrl: " +
-        data.tracks.items[0].preview_url +
-        "\nAlbum: " +
-        data.tracks.items[0].album.name
-    );
-  });
-} else if (process.argv[2] === "concert-this") {
-  var query =
-    "https://rest.bandsintown.com/artists/" +
-    input +
-    "/events?app_id=codingbootcamp";
-  //bandsintown API
-  request(query, function(err, response, data) {
-    // console.log(data);
-    var formattedData = JSON.parse(data);
-    var dateTime = moment(formattedData[0].datetime).format("MM/DD/YYYY");
-    console.log(
-      "Venue: " +
-        formattedData[0].venue.name +
-        "\nLocation: " +
-        formattedData[0].venue.city +
-        "\nDate: " +
-        dateTime
-    );
-  });
-} else if (process.argv[2] === "movie-this") {
-  var query = "http://www.omdbapi.com?apikey=trilogy&t=" + input;
-  //OMDBAPI
-  request(query, function(err, response, data) {
-    var formattedData = JSON.parse(data);
-    console.log(
-      "Year: " +
-        formattedData.Year +
-        "\nimdbRating: " +
-        formattedData.imdbRating +
-        "\nRotten Tomato: " +
-        formattedData.Ratings[1].Value +
-        "\nCountry: " +
-        formattedData.Country +
-        "\nLanguage: " +
-        formattedData.Language +
-        "\nPlot: " +
-        formattedData.Plot +
-        "\nActors: " +
-        formattedData.Actors
-    );
-  });
-} else if (process.argv[2] === "do-what-it-says") {
-  //random.txt
-  //fs.readFile(path[, options], callback)
-  fs.readFile("random.txt", "utf-8", function(err, data) {
-    if (err) throw err;
-    console.log(data);
-    var random = data.split(",")
-    console.log(random);
-  });
+function doSomething(command, input) {
+  if (command === "spotify-this-song") {
+    //spotify search requires
+    var query = {
+      type: "track",
+      query: input
+      // limit: 1
+    };
+
+    spotify.search(query, function(err, data) {
+      //   console.log(data.tracks.items);
+      console.log(
+        "Artist(s): " +
+          data.tracks.items[0].artists[0].name +
+          "\nSong's Name: " +
+          data.tracks.items[0].name +
+          "\nPreviewUrl: " +
+          data.tracks.items[0].preview_url +
+          "\nAlbum: " +
+          data.tracks.items[0].album.name
+      );
+    });
+  } else if (command === "concert-this") {
+    var query =
+      "https://rest.bandsintown.com/artists/" +
+      input +
+      "/events?app_id=codingbootcamp";
+    //bandsintown API
+    request(query, function(err, response, data) {
+      // console.log(data);
+      var formattedData = JSON.parse(data);
+      var dateTime = moment(formattedData[0].datetime).format("MM/DD/YYYY");
+      console.log(
+        "Venue: " +
+          formattedData[0].venue.name +
+          "\nLocation: " +
+          formattedData[0].venue.city +
+          "\nDate: " +
+          dateTime
+      );
+    });
+  } else if (command === "movie-this") {
+    var query = "http://www.omdbapi.com?apikey=trilogy&t=" + input;
+    //OMDBAPI
+    request(query, function(err, response, data) {
+      var formattedData = JSON.parse(data);
+      console.log(
+        "Title: " +
+          formattedData.Title +
+          "\nYear: " +
+          formattedData.Year +
+          "\nimdbRating: " +
+          formattedData.imdbRating +
+          "\nRotten Tomato: " +
+          formattedData.Ratings[1].Value +
+          "\nCountry: " +
+          formattedData.Country +
+          "\nLanguage: " +
+          formattedData.Language +
+          "\nPlot: " +
+          formattedData.Plot +
+          "\nActors: " +
+          formattedData.Actors
+      );
+    });
+  } else if (command === "do-what-it-says") {
+    //random.txt
+    //fs.readFile(path[, options], callback)
+    fs.readFile("random.txt", "utf-8", function(err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+
+        var randomArray = data.split(",");
+        command = randomArray[0];
+
+        input = randomArray[1];
+        console.log(command, input);
+
+        doSomething(command, input);
+      }
+    });
+  }
 }
